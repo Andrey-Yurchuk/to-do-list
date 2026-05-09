b038f63f3f037a85d76ce0291f63013f7198dfdb
Andrey Yurchuk <r1430@tut.by>
2026-05-09 09:16:47 +0300

    feat: add tasks migration and Task model

---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class,
+        ];
+    }
+}
